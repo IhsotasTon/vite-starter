f7678d2f3a38a7c782b3ea5d0cdc397807be6f7e
IhsotasTon <93902066+IhsotasTon@users.noreply.github.com>
2023-06-13 18:49:27 +0800

    modified:   .gitignore 	modified:   canister_ids.json 	modified:   index.html 	deleted:    src/App.tsx 	modified:   src/components/ui/input.tsx 	deleted:    src/favicon.svg 	deleted:    src/lib/utils.ts 	deleted:    src/logo.svg 	deleted:    src/main.tsx 	modified:   yarn-error.log

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,8 @@ function App() {
   return (
     <div>
       <div onClick={increasePopulation}>increase</div>
-      <h1>{data.name}</h1>
+      <h1>{data.name
+      }</h1>
       <p>{data.description}</p>
       <strong>👀 {data.subscribers_count}</strong>{' '}
       <strong>✨ {data.stargazers_count}</strong> <strong>🍴 {data.forks_count}</strong>
